3f9251218bf1316deeb075df10a3c7e075a70202
SHAWN <shawnhan1999@hotmail.com>
2019-05-17 22:35:57 -0400

    Try to resolve github conflict

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ driver = p.set_up("https://www.aircanada.com/ca/en/aco/home.html")
 # p.oneway_search(driver, 'YYZ', 'YOW', 29, 7, 2019)
 p.round_trip_search(driver, 'YYZ', 'YOW', 6, 7, 2019, 8, 8, 2019)
 
+# quit
 driver.quit()
