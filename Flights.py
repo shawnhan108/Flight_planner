f8235d40e05697e2e7dfddb2b381503ea725a730
SHAWN <shawnhan1999@hotmail.com>
2019-05-26 00:03:24 -0400

    Finished function to update SQL DB for oneway flights. Applied filters to arrays before storing records. Completed notification by amount.
    Next step:
    Complete notification by diff and trend. Consult details.
    alpha testing objects and related functions for oneway flights.

--- Flights.py
+++ Flights.py
@@ -1,4 +1,5 @@
 import datetime
+from datetime import datetime
 
 import mysql.connector
 
@@ -21,13 +22,12 @@ class Flight_oneway:
         my_cursor = mydb.cursor()
         self.username = username
         self.track_id = 1 + my_cursor.execute('SELECT MAX(track_id) FROM User_info')
-        self.table_name = 'tb' + str(self.track_id)
+        self.table_name = 'tb_' + str(self.track_id)
         self.depart = depart
         self.dest = dest
         self.depart_date = depart_date
         self.filters = Filters()
         self.notifications = Notification()
-        self.notify = False
 
     # store an object information in table User_info
     def push_info(self):
@@ -59,16 +59,54 @@ class Flight_oneway:
             for days in self.filters.day_filter:
                 if flight_day == days:
                     flight_info = []
-                    break
+                    return flight_info
 
-        if self.filters.depart_time_filter_switch():
-            pass
-            # mylist = [1, 1, 2, 3, 4, 5, 6, 6, 8, 9]
-            # mynewlist = list(filter(lambda x: x % 2 == 0, mylist))
+        if flight_info & self.filters.depart_time_filter_switch():
+            start = datetime.strptime(self.filters.depart_time_filter[0], '%H:%M').time()
+            end = datetime.strptime(self.filters.depart_time_filter[1], '%H:%M').time()
+            if start < end:
+                flight_info = list(filter(
+                    lambda x: (datetime.strptime(x[1][0], '%H:%M').time() > start) and (
+                            datetime.strptime(x[1][0], '%H:%M').time() < end), flight_info))
+            else:
+                flight_info = list(filter(lambda x: (datetime.strptime(x[1][0], '%H:%M').time() > start) or (
+                        datetime.strptime(x[1][0], '%H:%M').time() < end), flight_info))
+
+        if flight_info & self.filters.arrive_time_filter_switch():
+            start = datetime.strptime(self.filters.arrive_time_filter[0], '%H:%M').time()
+            end = datetime.strptime(self.filters.arrive_time_filter[1], '%H:%M').time()
+            if start < end:
+                flight_info = list(filter(
+                    lambda x: (datetime.strptime(x[1][2], '%H:%M').time() > start) and (
+                            datetime.strptime(x[1][2], '%H:%M').time() < end), flight_info))
+            else:
+                flight_info = list(filter(lambda x: (datetime.strptime(x[1][2], '%H:%M').time() > start) or (
+                        datetime.strptime(x[1][2], '%H:%M').time() < end), flight_info))
+
+        if flight_info & self.filters.price_amount_filter_switch():
+            if self.filters.price_amount_filter[0] == 'Economy':
+                flight_info = list(
+                    filter(lambda x: x[2][1] != 'NULL' and int(x[2][0]) <= self.filters.price_amount_filter[1],
+                           flight_info))
+            else:
+                flight_info = list(
+                    filter(lambda x: x[2][1] != 'NULL' and (int(x[2][1]) <= self.filters.price_amount_filter[1]),
+                           flight_info))
 
         return flight_info
 
-
+    def check_notify(self, flight_info):
+        if self.notifications.amount_notify():
+            if self.notifications.amount[0] == 'Economy':
+                filtered_lst = list(
+                    filter(lambda x: x[2][0] != 'NULL' and (int(x[2][0]) <= self.notifications.amount[1], flight_info)))
+                if filtered_lst:
+                    self.notifications.notify_display(filtered_lst)
+            else:
+                filtered_lst = list(
+                    filter(lambda x: x[2][1] != 'NULL' and (int(x[2][1]) <= self.notifications.amount[1], flight_info)))
+                if filtered_lst:
+                    self.notifications.notify_display(filtered_lst)
 
     def store_info(self):
         mydb = mysql.connector.connect(
@@ -84,6 +122,19 @@ class Flight_oneway:
                                       int(self.depart_date[6:]))
 
         # apply filters to the flight_info.
+        flight_info = self.apply_filters(flight_info)
+
+        # notify user if there are flights that satisfy their requirements.
+
+        # store flight_info into db.
+        # table_name columns: Depart, Dest, Date, Depart_time, Arrive_time, Eco_price, Bus_price
+        for flight in flight_info:
+            record = 'INSERT INTO {0} VALUES ({1}, {2}, {3}, {4}, {5}, {6}, {7});'.format(self.table_name, self.depart,
+                                                                                          self.dest, self.depart_date,
+                                                                                          flight[1][0], flight[1][2],
+                                                                                          int(flight[2][0]),
+                                                                                          int(flight[2][1]))
+            my_cursor.execute(record)
 
 
 class Filters:
@@ -122,7 +173,7 @@ class Filters:
     def insert_day_filter(self, excluded_days):
         self.day_filter = excluded_days
 
-    # time_range in the form of military time, e.g. [15:00, 21:00]
+    # time_range in the form of military time, e.g. [15, 21] int, int
     def insert_depart_time_filter(self, time_range):
         self.depart_time_filter = time_range
 
@@ -138,12 +189,12 @@ class Filters:
 class Notification:
 
     def __init__(self):
-        self.amount = -1
+        self.amount = []
         self.diff = 10000000
         self.trend = 10000000
 
     def amount_notify(self):
-        if self.amount == -1:
+        if not self.amount:
             return False
         else:
             return True
@@ -160,12 +211,16 @@ class Notification:
         else:
             return True
 
-    # limit must be a positive integer
-    def insert_amount(self, limit):
-        self.amount = limit
+    # limit must be a positive integer; class_type (string) is either 'Economy' or 'Business'
+    def insert_amount(self, class_type, limit):
+        self.amount = [class_type, limit]
 
-    def insert_diff(self, limit):
-        self.diff = limit
+    def insert_diff(self, class_type, limit):
+        self.diff = [class_type, limit]
 
-    def insert_trend(self, limit):
-        self.trend = limit
+    def insert_trend(self, class_type, limit):
+        self.trend = [class_type, limit]
+
+    # display_lst contains flight info that satisfies user's request, now display to UI.
+    def notify_display(self, display_lst):
+        pass
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import airportcodes
 import searchAirport
 import SearchDriver
+from datetime import datetime
+import time
+
 
 # print(airportcodes.giveCode("York Landing, MB"))
 
 # print(searchAirport.airportSearch("To"))
 # print(SearchDriver.retrieve_oneway('YOW', 'YVR', 5, 9, 2019))
-
-# [[['Ottawa', 'Vancouver'], ['07:00', 'Non-stop - 5hr12m', '09:12'], ['338', '1337']], [['Ottawa', 'Vancouver'], ['08:45', 'Non-stop - 5hr12m', '10:57'], ['338', '1337']], [['Ottawa', 'Vancouver'], ['17:50', 'Non-stop - 5hr21m', '20:11'], ['338', '1337']], [['Ottawa', 'Vancouver', 'YYC'], ['16:35', '1 Stop - 6hr38m', '20:13'], ['338', '1479']], [['Ottawa', 'Vancouver', 'YYC'], ['06:25', '1 Stop - 6hr43m', '10:08'], ['338', '1479']], [['Ottawa', 'Vancouver', 'YYZ'], ['07:00', '1 Stop - 6hr58m', '10:58'], ['343', '897']], [['Ottawa', 'Vancouver', 'YYZ'], ['16:00', '1 Stop - 6hr58m', '19:58'], ['NULL', '1840']], [['Ottawa', 'Vancouver', 'YYZ'], ['06:00', '1 Stop - 7hrs', '10:00'], ['497', '897']], [['Ottawa', 'Vancouver', 'YUL'], ['12:00', '1 Stop - 7hr04m', '16:04'], ['NULL', '2647']], [['Ottawa', 'Vancouver', 'YYZ'], ['08:00', '1 Stop - 7hr07m', '12:07'], ['343', '1484']], [['Ottawa', 'Vancouver', 'YYZ'], ['09:00', '1 Stop - 7hr07m', '13:07'], ['493', '1479']], [['Ottawa', 'Vancouver', 'YYZ'], ['10:00', '1 Stop - 7hr07m', '14:07'], ['343', '1484']], [['Ottawa', 'Vancouver', 'YYZ'], ['14:00', '1 Stop - 7hr07m', '18:07'], ['343', '1484']], [['Ottawa', 'Vancouver', 'YYZ'], ['06:00', '1 Stop - 7hr13m', '10:13'], ['343', '1484']], [['Ottawa', 'Vancouver', 'YYZ'], ['18:00', '1 Stop - 7hr13m', '22:13'], ['585', '2291']], [['Ottawa', 'Vancouver', 'YUL'], ['19:05', '1 Stop - 7hr14m', '23:19'], ['343', '897']], [['Ottawa', 'Vancouver', 'YYZ'], ['15:00', '1 Stop - 7hr15m', '19:15'], ['NULL', '1840']], [['Ottawa', 'Vancouver', 'YYC'], ['06:25', '1 Stop - 7hr20m', '10:45'], ['585', '897']], [['Ottawa', 'Vancouver', 'YYZ'], ['11:00', '1 Stop - 7hr22m', '15:22'], ['NULL', '1840']], [['Ottawa', 'Vancouver', 'YYZ'], ['12:00', '1 Stop - 7hr22m', '16:22'], ['581', '1479']]]
 
